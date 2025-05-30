d9ee8aad36224694e9b8b0b51658890d7f14b3ca
sibeldogru <sibeldogru@gmail.com>
2025-05-30 14:45:01 +0300

    Add project files.

---
+++ TatilArkadasim/Program.cs
@@ -0,0 +1,93 @@
+﻿
+using System.Collections;
+
+string choice1 = "Bodrum";
+string choice2 = "Marmaris";
+string choice3 = "Çeşme";
+
+string answer = "evet";
+
+do
+{
+    Console.WriteLine("Yol Arkadaşı Uygulamasına Hoş Geldiniz!");
+    Console.WriteLine("Lütfen tatil lokasyonunuzun ismini yazınız.");
+    Console.WriteLine("1 - Bodrum");
+    Console.WriteLine("2 - Marmaris");
+    Console.WriteLine("3 - Çeşme");
+
+    string usersChoice = Console.ReadLine().ToLower();
+
+
+    while (usersChoice != choice1.ToLower() && usersChoice != choice2.ToLower() && usersChoice != choice3.ToLower())
+    {
+        Console.WriteLine("Geçersiz bir lokasyon girdiniz.");
+        Console.WriteLine("Lütfen tatil lokasyonunuzun ismini yazınız.");
+        Console.WriteLine("1 - Bodrum");
+        Console.WriteLine("2 - Marmaris");
+        Console.WriteLine("3 - Çeşme");
+        usersChoice = Console.ReadLine().ToLower();
+
+    }
+
+    Console.WriteLine("Tatile kaç kişi gideceksiniz?");
+    int guest = Convert.ToInt32(Console.ReadLine());
+
+    switch (usersChoice)
+    {
+        case "bodrum":
+            Console.WriteLine("Bodrum, Ege Denizi kıyısında yer alan popüler bir tatil beldesidir. Beyaz evleri, mavi denizi ve tarihi yapılarıyla ünlüdür. Bodrum'da denize girebilir, tekne turuna çıkabilir, tarihi yerleri gezebilir, gece hayatının tadını çıkarabilirsiniz.");
+            break;
+        case "marmaris":
+            Console.WriteLine("Marmaris, Ege ve Akdeniz’in birleştiği noktada bulunan popüler bir tatil beldesidir. Doğal koyları, yat limanı ve çam ormanlarıyla çevrili yapısıyla öne çıkar. Marmaris'te denize girebilir, tekne turuna çıkabilir, doğayla iç içe olabilir, Marmaris Kalesi'ni gezebilirsiniz.");
+            break;
+        case "çeşme":
+            Console.WriteLine("Çeşme, İzmir’in batısında yer alan, Ege Denizi kıyısında ünlü bir tatil beldesidir. Rüzgarlı havası, plajları ve lüks tatil köyleriyle tanınır. Çeşme'de sörf yapabilir, denize gireilir, gece hayatının tadını çıkarabilir, lezzet keşfi yapabilirsiniz.");
+            break;
+    }
+
+    Console.WriteLine("Tatile ne şekilde gideceksiniz?");
+    Console.WriteLine("1 - Kara yolu ( Kişi başı ulaşım tutarı gidiş-dönüş 1500 TL )");
+    Console.WriteLine("2 - Hava yolu ( Kişi başı ulaşım tutarı gidiş-dönüş 4000 TL)");
+    Console.WriteLine("Lütfen cevabı 1 ya da 2 olarak giriniz.");
+    int transportationChoice = Convert.ToInt32(Console.ReadLine());
+    int transportationChoice1 = 1;
+    int transportationChoice2 = 2;
+
+
+    int j = 0;
+    while (transportationChoice != transportationChoice1 && transportationChoice != transportationChoice2)
+    {
+        Console.WriteLine("Geçersiz bir ulaşım şekli girdiniz.Lütfen tekrar giriniz");
+        Console.WriteLine("1 - Kara yolu ( Kişi başı ulaşım tutarı gidiş-dönüş 1500 TL )");
+        Console.WriteLine("2 - Hava yolu ( Kişi başı ulaşım tutarı gidiş-dönüş 4000 TL)");
+        Console.WriteLine("Lütfen cevabı 1 ya da 2 olarak giriniz.");
+        transportationChoice = Convert.ToInt32(Console.ReadLine());
+        j++;
+    }
+
+    int transportationPrice1 = 1500;
+    int transportationPrice2 = 4000;
+
+    if (transportationChoice == transportationChoice1)
+    {
+        Console.WriteLine("Tatil gideriniz şu şekilde -> " + transportationPrice1 * guest);
+    }
+    else
+        Console.WriteLine("Tatil gideriniz şu şekilde ->" + transportationPrice2 * guest);
+
+    Console.WriteLine("Başka bir tatil planlamak istiyor musunuz? Lütfen evet ya da hayır yazınız.");
+    answer = Console.ReadLine().ToLower();
+
+} while (answer == "evet");
+
+Console.WriteLine("Tatil planlayıcısını kullandığınız için teşekkürler!");
+
+
+
+
+
+
+
+
+
+
